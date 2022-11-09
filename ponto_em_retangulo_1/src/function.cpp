ff2638d100e9d1888b375ad5c56fc4439bf66858
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2022-11-09 16:22:22 -0300

    Update function.cpp

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -5,9 +5,35 @@
  */
 
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
-{
-    // TODO: Coloque aqui seu código.
+{   
+    //INSIDE
+    if((P.x>IE.x) && (P.y>IE.y)){
+        if((P.x<SD.x) && (P.y<SD.y)){
+            return location_t::INSIDE;
+        }
+    }
 
+    //OUTSIDE
+    if((P.x<IE.x) || (P.y<IE.y)){
+        return location_t::OUTSIDE;
+    }
+    if((P.x>SD.x) || (P.y>SD.y)){
+        return location_t::OUTSIDE;
+    }
+
+    //BORDER
+    if((P.x==IE.x) && ((P.y>=IE.y) && (P.y<=SD.y))){
+        return location_t::BORDER;
+    }
+    if((P.y==IE.y) && ((P.x>=IE.x) && (P.x<=SD.x))){
+        return location_t::BORDER;
+    }
+    if((P.x==SD.x) && ((P.y>=IE.y) && (P.y<=SD.y))){
+        return location_t::BORDER;
+    }
+    if((P.y==SD.y) && ((P.x>=IE.x) && (P.x<=SD.x))){
+        return location_t::BORDER;
+    }
     // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
-}
+
+}   
