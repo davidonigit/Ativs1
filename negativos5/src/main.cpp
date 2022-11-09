96ddfa11d95b11b0c54280f55eb5107322d989fe
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2022-11-09 16:21:44 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -5,7 +5,16 @@ using std::cout;
 const int SIZE = 5; // input size.
 
 int main(void)
-{
+{   
+    int enter, result=0, i;
+    for(i=0;i<5;i++){
+        cin>>enter;
+        if(enter<0){
+            result+=1;
+        }
+    }
+
+    cout<<result;
     // TODO: Adicione aqui seu código.
 
     return 0;
