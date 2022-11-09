737afeb415e750881661c7bae171ecf68e2c7250
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2022-11-09 16:23:08 -0300

    Update main.cpp

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -21,7 +21,49 @@ using std::max;
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int x1,y1,x2,y2,x3,y3;
+    Ponto IE;
+    Ponto SD;
+    Ponto P;
 
+    while(cin>>std::ws>>x1>>y1>>x2>>y2>>x3>>y3){
+
+        //testa validade do retangulo
+        if((x1==x2) || (y1==y2)){
+            cout<<"invalid"<<endl;
+        }
+
+        if(x1<x2){
+            IE.x=x1;
+            SD.x=x2;
+        }else{
+            IE.x=x2;
+            SD.x=x1;
+        }
+
+        if(y1<y2){
+            IE.y=y1;
+            SD.y=y2;
+        }else{
+            IE.y=y2;
+            SD.y=y1;
+        }
+
+        P.x=x3;
+        P.y=y3;
+
+        
+        if(location_t::INSIDE==pt_in_rect(IE, SD, P)){
+            cout<<"inside"<<endl;
+        }
+        if(location_t::BORDER==pt_in_rect(IE, SD, P)){
+            cout<<"border"<<endl;
+        }
+        if(location_t::OUTSIDE==pt_in_rect(IE, SD, P)){
+            cout<<"outside"<<endl;
+        }
+
+
+    }
     return 0;
 }
